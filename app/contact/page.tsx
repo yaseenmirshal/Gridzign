4e0c6a790cd62514fbde16270e5e55eecb8f87d7
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2026-02-06 23:10:20 +0530

    updated late

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -64,6 +64,7 @@ export default function ContactPage() {
                       </div>
                     </div>
                   ))}
+                  
                 </div>
               </VelocityMarquee>
             </div>
